a1e151c1852fa861aa52e41052cb5e7b1450b8c7
Laudemir <nlaudemir@gmail.com>
2022-04-10 03:09:06 -0300

    feat: add ormconfigs for tests

--- src/__tests__/ormconfig.ts
+++ src/__tests__/ormconfig.ts
@@ -1,22 +1,7 @@
-import dotenv from "dotenv";
-import path from "path";
-import { ConnectionOptions } from "typeorm";
+import databaseOptions from "../database/ormconfig";
 
-dotenv.config();
+const dbOptions = databaseOptions as any;
 
-export default {
-  type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  synchronize: true,
-  logging: false,
-  entities: [path.join(__dirname, "../entities/**/*.*")],
-  migrations: [path.join(__dirname, "../migrations/**/*.*")],
-  cli: {
-    entitiesDir: path.join(__dirname, "../entities"),
-    migrationsDir: path.join(__dirname, "../migrations"),
-  },
-} as ConnectionOptions;
+dbOptions.host = "localhost";
+
+export default dbOptions;
