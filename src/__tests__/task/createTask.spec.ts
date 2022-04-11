8829f249a7346ffe20142fbb1bd84daa5fa02ca2
Laudemir <nlaudemir@gmail.com>
2022-04-11 17:04:27 -0300

    test: create test where create task with other user

--- src/__tests__/task/createTask.spec.ts
+++ src/__tests__/task/createTask.spec.ts
@@ -2,13 +2,14 @@ import request from "supertest";
 import { createConnection } from "typeorm";
 import { app } from "../../app";
 import dbOptions from "../../database/ormconfig";
-import { task, user } from "../mock";
+import { newUser, newUserLogin, task, user } from "../mock";
 import { userLogin } from "./../mock";
 let token: any = {};
 
 beforeAll(async () => {
   await createConnection(dbOptions);
   await request(app).post("/register").send(user);
+  await request(app).post("/register").send(newUser);
 });
 
 describe("Create Task", () => {
@@ -36,5 +37,18 @@ describe("Create Task", () => {
       .set("Authorization", `Token ${token.token}`);
     expect(response.body).not.toHaveProperty("id");
     expect(response.status).toBe(400);
+  });
+  it("Should be able to login", async () => {
+    const response = await request(app).post("/login").send(newUserLogin);
+    token = JSON.parse(response.text);
+    expect(response.status).toBe(200);
+  });
+  it("should be able to create task", async () => {
+    const response = await request(app)
+      .post("/task")
+      .send(task)
+      .set("Authorization", `Token ${token.token}`);
+    expect(response.body).toHaveProperty("id");
+    expect(response.status).toBe(201);
   });
 });
